5b97e80f0ebc5140e2acae4635ca9abfc225ca86
bbf1999 <39823691+bbf1999@users.noreply.github.com>
2018-06-11 16:08:51 +0800

    Desktop
    (cherry picked from commit 37d4e07c51051e740a55bcb3e862b58515b22538)

--- app/src/main/java/com/example/administrator/myapplication/Main3Activity.kt
+++ app/src/main/java/com/example/administrator/myapplication/Main3Activity.kt
@@ -3,6 +3,12 @@ package com.example.administrator.myapplication
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.widget.TextView
+import org.jetbrains.anko.button
+import org.jetbrains.anko.editText
+import org.jetbrains.anko.relativeLayout
+import org.jetbrains.anko.sdk15.listeners.onClick
+import org.jetbrains.anko.toast
+
 
 class Main3Activity : AppCompatActivity() {
 
@@ -13,7 +19,14 @@ class Main3Activity : AppCompatActivity() {
         var tt3 = findViewById<TextView>(R.id.textView3)
 
         tt3.text = this.intent.getStringExtra("name1")
-
+        relativeLayout {
+            var name = editText()
+            button("button by anko") {
+                onClick {
+                    toast("Hi,${name.text}!!")
+                }
+            }
+        }
         //var bundle =this.intent.extras
         //tt3.text=bundle.get("name").toString()
 
